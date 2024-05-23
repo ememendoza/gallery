bcd16aa1b56c8e475341923d5a075325e21444b3
ememendozar <ememendozar002@gmail.com>
2024-05-23 10:11:29 -0500

    Feat: Create getByCategory method

--- controllers/gallery.controller.js
+++ controllers/gallery.controller.js
@@ -1,48 +1,69 @@
-import getConnection from '../database/database.js';
-import Gallery from '../models/gallery.js';
-import { uploadFile, getFiles, getFile, downloadFile, getFileURL } from '../config/s3.js';
+import getConnection from "../database/database.js";
+import Gallery from "../models/gallery.js";
+import {
+  uploadFile,
+  getFiles,
+  getFile,
+  downloadFile,
+  getFileURL,
+} from "../config/s3.js";
 
 const postPicture = async (req, res) => {
-    const { name, size } = req.files.file;
-    const category = req.category;
+  const { name, size } = req.files.file;
+  const category = req.category;
 
-    const picture = new Gallery(name, size, category);
-    const connection = await getConnection();
-    const dbResult = await connection.query("INSERT INTO gallery SET ?", picture);
-    const s3Result = await uploadFile(req.files.file);
+  const picture = new Gallery(name, size, category);
+  const connection = await getConnection();
+  const dbResult = await connection.query("INSERT INTO gallery SET ?", picture);
+  const s3Result = await uploadFile(req.files.file);
 
-    res.json({
-        "Database Response": dbResult,
-        "Bucket Response": s3Result
-    });
-}
+  res.json({
+    "Database Response": dbResult,
+    "Bucket Response": s3Result,
+  });
+};
 
 const getGallery = async (req, res) => {
-    const result = await getFiles();
-    res.json(result.Contents);
-}
+  const result = await getFiles();
+  res.json(result.Contents);
+};
 
-const getGalleryByCategory = async(req, res) => {
-    
-}
+const getGalleryByCategory = async (req, res) => {
+  const category = req.category;
+  const connection = await getConnection();
+  const result = await connection.query(
+    "SELECT name FROM gallery WHERE category =?",
+    category
+  );
+
+  const galleryUrls = await Promise.all(
+    result.map(async (gallery) => {
+      const url = await getFileURL(gallery.name);
+      return { url };
+    })
+  );
+
+  res.json(galleryUrls);
+};
 
 const findGallery = async (req, res) => {
-    const result = await getFileURL(req.params.fileName);
-    res.json({
-        "url": result
-    });
-}
+  const result = await getFileURL(req.params.fileName);
+  res.json({
+    url: result,
+  });
+};
 
 const downloadGallery = async (req, res) => {
-    await downloadFile(req.params.fileName);
-    res.json({
-        "message": "File Downloaded"
-    });
-}
+  await downloadFile(req.params.fileName);
+  res.json({
+    message: "File Downloaded",
+  });
+};
 
 export const methods = {
-    postPicture,
-    getGallery,
-    findGallery,
-    downloadGallery
+  postPicture,
+  getGallery,
+  getGalleryByCategory,
+  findGallery,
+  downloadGallery,
 };
--- routes/gallery.route.js
+++ routes/gallery.route.js
@@ -6,6 +6,7 @@ const router = Router();
 router.post("/files", galleryController.postPicture);
 router.get("/files", galleryController.getGallery);
 router.get("/file/:fileName", galleryController.findGallery);
+router.get("/file/:category", galleryController.getGalleryByCategory);
 router.get("/downloadFile/:fileName", galleryController.downloadGallery);
 
 export default router;
